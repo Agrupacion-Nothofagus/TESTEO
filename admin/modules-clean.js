50d841defe8dcb46c3dd409d2455c7e054d615fe
Luxetanus <luciano.antonio.leal.ulloa@gmail.com>
2026-07-21 00:13:24 -0400

    Load active socios cuotas reference layout

--- admin/modules-clean.js
+++ admin/modules-clean.js
@@ -21,6 +21,7 @@ import './tesoreria-comprobantes.js?v=20260703-clean';
 import './tesoreria-cuotas-submit-guard.js?v=20260707';
 import './tesoreria-cuotas.js?v=20260704-cuotas';
 import './tesoreria-cuotas-nomina.js?v=20260708';
+import './tesoreria-cuotas-activos-layout-loader.js?v=20260710-activos-layout-ref';
 import './tesoreria-cuotas-benefactores.js?v=20260710-table-fit';
 import './tesoreria-cuotas-benefactores-layout-refresh.js?v=20260710-table-fit';
 import './tesoreria-cuotas-benefactores-modal-fix-loader.js?v=20260710-modal-fit';
